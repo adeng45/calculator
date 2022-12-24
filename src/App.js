51b53cd6d6e362f1665c1e825797332b6041074d
Anthony Deng <andeng2@berkeley.edu>
2022-12-24 00:39:04 -0800

    added functionality

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
+import './App.css';
+import  { useState } from 'react';
+
+
 function App() {
 
-  const sideOps = ["+", "-", "*"];
+  const [res, setRes] = useState("");
+
+  //Generates the middle 3 rows.
+  const operators = ['+', '-', '*', '/', '^', '%', '.'];
+
+  const del = (i) => {
+    if (res === '') {
+      return;
+    }
+    setRes(res.slice(0, i));
+  }
+
+  const evaluate = () => {
+    setRes(eval(res).toString());
+  }
 
   const generate = () => {
 
@@ -9,35 +27,61 @@ function App() {
     for (let i = 0; i < 3; i++) {
       for (let j = 1; j < 4; j++) {
         buttons.push(
-          <button key={3 * i + j}>{3 * i + j}</button>
+          <button className='digit' onClick={() => updateRes((3 * i + j).toString())}>{3 * i + j}</button>
         )
       }
       buttons.push(
-        <button key={sideOps[i]}>{sideOps[i]}</button>
+        <button className='op' onClick={() => updateRes(operators[i])}>{operators[i]}</button>
       )
     }
 
     return buttons;
   }
 
+  const updateRes = (value) => {
+
+    //Don't want to start off with an operator.
+    if (res === '' && operators.includes(value))  {
+      return;
+    }
+
+    //No consecutive operators.
+    if (operators.includes(value) && operators.includes(res.slice(-1))) {
+      return;
+    }
+
+    //Leading 0
+    if (res === '0' && !operators.includes(value)) {
+      setRes(value);
+      return;
+    }
+    
+    setRes(res + value);
+  }
+
   return (
     <div className="App">
       <div className="calculator">
         <div className="display">
-          0
+        <span>{res}</span>
         </div>
 
-        <button>C</button>
-        <button>^</button>
-        <button>%</button>
-        <button>DEL</button>
-        
-        {generate()}
-  
-        <button>0</button>
-        <button>.</button>
-        <button>=</button>
-        <button>/</button>
+        <div className="buttons">
+          {/* Top row */}
+          <button className='op' onClick={() => del(0)}>C</button>
+          <button className='op' onClick={() => updateRes('^')}>^</button>
+          <button className='op' onClick={() => updateRes('%')}>%</button>
+          <button className='op' onClick={() => del(-1)}>DEL</button>
+          
+          {/* Middle 3 rows. */}
+          {generate()}
+    
+          {/* Bottom row */}
+          <button className='digit' onClick={() => updateRes('0')}>0</button>
+          <button className='op' onClick={() => updateRes('.')}>.</button>
+          <button className='op' onClick={() => evaluate()}>=</button>
+          <button className='op' onClick={() => updateRes('/')}>/</button>
+        </div>
 
       </div>
     </div>
